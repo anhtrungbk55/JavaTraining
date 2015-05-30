402d5ccfe8d2e856b8ee6e746a85b7956656b95a
anhtrungbk55 <anhtrungbk55@gmail.com>
2015-05-30 11:18:45 +0700

    test

--- Documents/workspace/JavaTraining/src/com/bkav/training/week1/sesssion6/T.java
+++ Documents/workspace/JavaTraining/src/com/bkav/training/week1/sesssion6/T.java
@@ -2,7 +2,7 @@ package com.bkav.training.week1.sesssion6;
 
 public class T {
 	public String toString(){
-		System.out.println("Them du lieu");
+		System.out.println("Them du lieu x");
 		return null;
 	}
 }
