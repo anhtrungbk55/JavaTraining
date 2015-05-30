783b4e3b337af8f365b83ae000c83ea6a297d360
anhtrungbk55 <anhtrungbk55@gmail.com>
2015-05-30 16:46:18 +0700

    saturday

--- Documents/workspace/JavaTraining/src/com/bkav/training/week1/sesssion7/TrigonometricSeries.java
+++ Documents/workspace/JavaTraining/src/com/bkav/training/week1/sesssion7/TrigonometricSeries.java
@@ -1,5 +1,19 @@
 package com.bkav.training.week1.sesssion7;
 
 public class TrigonometricSeries {
-	
+	public static double sin(double x, int numTerms){
+		double sin = 0;
+		for (int i = 1; i < numTerms; i++) {
+			 
+		}
+		return sin;
+	}
+	public static double cos(double x, int numTerms){
+		double cos = 0;
+		return cos;
+	}
+	public static  int fac(int num) {
+		int fac = 1;
+		return fac;
+	}
 }
